2561c31124a0c360eafa34f3cfc517d107f263a9
Fernando Rodriguez <fernando.rdzcrz@gmail.com>
2021-07-23 07:39:04 -0500

    rafactor structure folders

--- js/index.js
+++ js/index.js
@@ -6,9 +6,9 @@ let saludo=$(".saludo")
 saludo.innerHTML=separateText("Hola, mi nombre es"," ")
 
 let h1 = $(".titulo");
-h1.innerHTML = separateText("Fernando Rodriguez");
+h1.innerHTML ='<span class="sub-title">'+ separateText("Fernando")+'</span><span class="sub-title">'+separateText("Rodriguez")+"</span>";
 
-let description = $(".description");
+let description = $(".info");
 description.innerHTML = separateText(`Soy un estudiante de Desarrollo de software apasionado por la tecnologia al que le encanta el desarrollo de videojuegos.`," ");
 
 
@@ -49,7 +49,7 @@ let barFg=$(".bar-fg")
 document.querySelectorAll(".skill").forEach((e)=>{
     e.addEventListener("mouseover",(e)=>{
         barFg.style.width=e.target.dataset.level;
-        tooltipText.innerHTML=e.target.dataset.level;
+        tooltipText.innerHTML=e.target.dataset.name+" "+e.target.dataset.level;
         tooltip.style.opacity="1"
         tooltip.style.top = (e.pageY+10).toString() + "px" ;
         tooltip.style.left = (e.pageX+10).toString() + "px";
